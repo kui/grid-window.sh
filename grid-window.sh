42abfedf07605cca2bb8914056a52bf1509375b9
Keiichiro Ui <keiichiro.ui@gmail.com>
2013-02-25 22:41:31 +0900

    add get desktop size with wmctrl

--- grid-window.sh
+++ grid-window.sh
@@ -10,15 +10,23 @@
 # settings
 VERTICAL_GRID_NUM=3
 HORIZONTAL_GRID_NUM=6
-
-XDOTOOL="xdotool"
 DEBUG=true
 
+XDOTOOL="xdotool"
+WMCTRL="wmctrl"
+
+CURRENT_DESKTOP_NUM=
+DESKTOP_WIDTH=
+DESKTOP_HEIGHT=
+ACTIVE_WINDOW_WIDTH=
+ACTIVE_WINDOW_HIGHT=
+
 main() {
-    get_active_window_geometry
-    resize '100%' '100%'
-    get_active_window_geometry
-    resize '60%' '100%'
+    get_desktop_size
+    # get_active_window_geometry
+    # resize '100%' '100%'
+    # get_active_window_geometry
+    # resize '60%' '100%'
 }
 
 help(){
@@ -37,13 +45,46 @@ e(){
     p "ERROR: ${@}" >&2
 }
 
+get_active_desktop(){
+    CURRENT_DESKTOP_NUM="`"$XDOTOOL" get_desktop`"
+}
+
+get_desktop_size(){
+    get_desktop_size_with_wmctrl || get_desktop_size_with_xdotool
+}
+
+get_desktop_size_with_wmctrl(){
+    get_active_desktop
+   local dsize="`"$WMCTRL" -d | grep "^$CURRENT_DESKTOP_NUM" |\
+                  sed --quiet 's/.* \([0-9]\+x[0-9]\+\) .*/\1/p'`"
+
+   [ -z "$dsize" ] && d "cannot get desktop size with wmctrl" && return 1
+
+   DESKTOP_WIDTH="`echo $dsize | cut -dx -f1`"
+   DESKTOP_HEIGHT="`echo $dsize | cut -dx -f2`"
+
+   d "$FUNCNAME: width=${DESKTOP_WIDTH}, height=${DESKTOP_HEIGHT}"
+}
+
+get_desktop_size_with_xdotool(){
+    get_current_window_size
+    resize '100%' '100%'
+    get_active_window_geometry
+}
+
+get_current_window_size(){
+    get_active_window_geometry
+    ACTIVE_WINDOW_WIDTH=$WIDTH
+    ACTIVE_WINDOW_HIGHT=$HIGHT
+}
+
 get_active_window_geometry(){
-    eval `$XDOTOOL getactivewindow getwindowgeometry --shell`
+    eval `"$XDOTOOL" getactivewindow getwindowgeometry --shell`
     d "active window geometry: ${WIDTH}x${HEIGHT}+${X}+${Y}"
 }
 
 resize(){
-    $XDOTOOL getactivewindow windowsize "$1" "$2"
+    "$XDOTOOL" getactivewindow windowsize "$1" "$2"
 }
 
 $DEBUG && set -x
